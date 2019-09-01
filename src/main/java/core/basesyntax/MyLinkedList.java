3511bb5e40076a03e396b99a91cfb4790cac390c
Sviatoslav <s.granovskiy@gmail.com>
2019-09-01 14:27:17 +0300

    work in progress

--- src/main/java/core/basesyntax/MyLinkedList.java
+++ src/main/java/core/basesyntax/MyLinkedList.java
@@ -3,25 +3,63 @@ package core.basesyntax;
 import java.util.List;
 
 public class MyLinkedList<T> implements MyLinkedListInterface<T> {
+    private int size;
+    private Node<T> head;
+    private Node<T> tail;
+
     @Override
     public void add(T value) {
-
+        Node node = new Node();
+        node.element = value;
+        node.next = null;
+        //System.out.println(node);
+        //node = new Node<T>(value);
+        if (head == null) {
+            head = node;
+            size++;
+            System.out.println(head.element);
+        } else {
+            Node n = head;
+            while(n.next != null) {
+                n = n.next;
+                System.out.println(n.element);
+            }
+            size++;
+            n.next = node;
+        }
+        System.out.println(head.element);
     }
 
     @Override
     public void add(T value, int index) {
-
     }
 
     @Override
     public void addAll(List<T> list) {
-
     }
 
     @Override
     public T get(int index) {
-        return null;
+        Node currentNode = head;
+        T result = null;
+        while (currentNode.next != null) {
+            int i = 0;
+            if (i == index) {
+                result = (T) currentNode;
+            }
+            currentNode = currentNode.next;
+            i++;
+        }
+        return result;
     }
+
+/*        T result = null;
+        for (int i = 0; i <= index;  i++) {
+            if (index == i) {
+                result = (T) node.element;
+            }
+        }
+        return result;*/
 
     @Override
     public void set(T value, int index) {
@@ -40,11 +78,36 @@ public class MyLinkedList<T> implements MyLinkedListInterface<T> {
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private class Node<T> {
+        Node prev;
+        T element;
+        Node next;
+
     }
 }
+
+/*            Node<T> firstNode = new Node<>();
+            Node<T> currentNode = firstNode;
+            System.out.println(firstNode.element);
+            //currentNode.next = (T) newNode;
+            //currentNode.index++;*/
+
+//if (size != 0) {
+
+/*            for (int i = 0; i < size; i++){
+                if (index == i) {
+                    Node<T> newNode = new Node<>(value);
+                    currentNode.next = (T) newNode;
+                    //currentNode.index++;
+                    size++;
+                }
+            }*/
+//currentNode.prev = get(size - 1);
